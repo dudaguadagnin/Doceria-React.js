fcde82a395399eb54d6b244525dbb04c3d8a15cb
Eduarda <dudaguadagnin@unochapeco.edu.br>
2021-01-13 07:49:53 -0300

    adicionando estilo em sobre

--- src/Sobre.js
+++ src/Sobre.js
@@ -1,33 +1,44 @@
 import React, { Fragment } from 'react';
 import Header from './Header';
+import './Sobre.css'
 
-const Sobre = () =>{
+
+
+
+const Sobre = () => {
 
     return (
         <Fragment>
             <Header />
-            <h1>Sobre</h1>
-            <h2>Localizada no coração da cidade traz para o mercado o que
-					há de melhor de confeitaria. Fundada em 2019
-					, a Pão Doce já é destaque na cidade e conquista
-							  novos clientes a cada dia.</h2>
-            <p>Oferecemos profissionais experientes e antenados às mudanças no mundo
-                 da moda. O atendimento possui padrão de excelência e agilidade,
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+            <section className="sobre">
+                <div className="conteudo-sobre">
+                    <p className="texto">Localizada no coração da cidade traz para o mercado o que
+                    há de melhor de confeitaria.</p>
+                    <p className="texto2">Fundada em 2019, a Pão Doce já é destaque na cidade e conquista
+					novos clientes a cada dia.</p>
+                    <p className="texto3">Oferecemos profissionais experientes e antenados às mudanças no mundo
+                    da moda. O atendimento possui padrão de excelência e agilidade,
                   garantindo qualidade e satisfação dos nossos clientes.</p>
+                <div class="ilustracao"></div>
+                </div>
+                
+                <div class="beneficios">
+                    <h3 class="pp"> Beneficios</h3>
 
-                  
-                  <div class="beneficios">
-					<h3 class="pp"> beneficios</h3>
+                    <ul className="itens">
+                        <li><i class="tiny material-icons">face</i>Atendimento ao cliente</li>
+                        
+                        <li ><i class="tiny material-icons">loyalty</i>Espaço diferenciado</li>
 
-					<ul>
-						<li class="itens">atendimento ao cliente</li>
-						<li class="itens">espaço diferenciado</li>
-						<li class="itens">localizaçao</li>
-						<li class="itens">profissionais qualificados</li>
-					</ul>
-				</div>
-                  
-        </Fragment> 
+                        
+                        <li><i class="tiny material-icons">group</i>Profissionais qualificados</li>
+                        
+                        <li><i class="tiny material-icons">restaurant_menu</i>Produtos deliciosos e de qualidade!</li>
+                    </ul>
+                </div>
+            </section>
+        </Fragment>
     );
 }
 
